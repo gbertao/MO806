381f3bb1f75b35105aa17669a99e62f47c03cdf5
Giovanni <gbertao96@gmail.com>
2021-01-19 03:19:16 +0000

    Hypervisor ready and running the guest's payload

---
+++ payload.s
@@ -0,0 +1,33 @@
+	.code16
+.macro KCALL par0
+	mov $e_\par0, %esi
+	sub $s_\par0, %esi
+	mov $0x50, %dx
+	mov $0x41, %al
+	out %al, %dx
+s_\par0:
+	call \par0
+e_\par0:
+.endm
+
+.macro KRET
+	mov (%esp), %esi
+	mov $0x51, %dx
+	mov $0x41, %al
+	out %al, %dx
+	ret
+.endm
+
+	.global pl
+	.text
+pl:
+	KCALL func
+        hlt
+
+func:
+	mov $0xa, %ebx
+	KRET
+
+rop_tgt:
+	mov $0x0, %ebx
+	hlt
